dd14aa439443f90055c06ffb832d82a357bb85b9
Sandhya2711 <sandhya27sb@gmail.com>
2020-07-01 00:08:34 +0530

    testing build via jenkins

--- src/app/User.ts
+++ src/app/User.ts
@@ -4,10 +4,5 @@ export class User{
     userName:String;
     password:String;
 
-    // constructor(id:number,emailId:String, userName:String,password:String){
-    //      this.id=id;
-    //      this.emailId=emailId;
-    //      this.userName=userName;
-    //      this.password=password;
-    // }
+   //testing build via jenkins
 }
